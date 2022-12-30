c7f2e5a52dac206d18bfcbd7181c60bed0eb83c1
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2022-12-29 19:04:23 -0500

    update

--- src/Ball.java
+++ src/Ball.java
@@ -19,6 +19,31 @@ public class Ball extends Rectangle {
 		xVelocity = 0;
 		yVelocity = 0;
 	} // end of constructor
+	
+	// called whenever the movement of the ball changes in the y-direction (up/down)
+	public void setYDirection(int yDirection) {
+		yVelocity = yDirection;
+	} // end of setYDirection
+	
+	// called whenever the movement of the ball changes in the x-direction
+	// (left/right)
+	public void setXDirection(int xDirection) {
+		xVelocity = xDirection;
+	} // end of setXDirection
+	
+	// called frequently from GamePanel class
+	// updates the current location of the ball
+	public void move() {
+		x += xVelocity;
+		y += yVelocity;
+	} // end of move
+	
+	// called frequently from the GamePanel class
+	// draws the current location of the ball to the screen
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillOval(x, y, BALL_DIAMETER, BALL_DIAMETER);
+	} // end of draw	
 
 	// when a key is pressed, call on move
 	public void keyPressed(KeyEvent e) {
