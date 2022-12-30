76d5f18bf7fcf281d193f382ce9cac792482a72e
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2022-12-29 19:01:49 -0500

    update

--- src/Ball.java
+++ src/Ball.java
@@ -1,5 +1,28 @@
+// Anne and Atisa
+// December 29, 2022
+// Ball
+// This class will manage the properties and behaviours of the ball.
+
 import java.awt.*;
+import java.awt.event.*;
 
 public class Ball extends Rectangle {
+  
+  // variable declaration
+	public int xVelocity, yVelocity;
+	public static final int SPEED = 4; // movement speed of ball
+	public static final int BALL_DIAMETER = 20; // size of ball
+
+	// constructor creates ball at given location with given dimensions. also initializes velocities
+	public RandomBall(int x, int y) {
+		super(x, y, BALL_DIAMETER, BALL_DIAMETER);
+		xVelocity = 0;
+		yVelocity = 0;
+	} // end of constructor
+
+	// when a key is pressed, call on move
+	public void keyPressed(KeyEvent e) {
+		move();
+	} // end of keyPressed
 
 }
