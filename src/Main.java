a268d5b0c894120e494a985557b7cbccd7e9912e
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2023-01-03 21:00:18 -0500

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+// Anne and Atisa
+// January 3, 2022
+// Main
+/* Main class starts the game
+All it does is run the constructor in GameFrame class
+This is a common technique among coders to keep things organized (and handy when coding in repl.it since we're forced to call a class Main, which isn't always very descriptive)
+*/
+
 public class Main {
-    public static void main(String[] args) {
-
-
-
-    }
-}
+	public static void main(String[] args) {
+		// call on GameFrame
+		new GameFrame();
+	} // end of main method
+} // end of Main class
