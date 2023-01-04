4ecf8654a2ad2eb5d8e02be2608ed1a16eec4232
Atisa Wang <atisa.wang@student.tdsb.on.ca>
2023-01-03 21:02:27 -0500

    main (main)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-// Anne and Atisa
-// January 3, 2022
-// Main
-/* Main class starts the game
-All it does is run the constructor in GameFrame class
-This is a common technique among coders to keep things organized (and handy when coding in repl.it since we're forced to call a class Main, which isn't always very descriptive)
-*/
+/*
+Anne Liu & Atisa Wang
+03/01/22
+Main
+ */
 
 public class Main {
-	public static void main(String[] args) {
-		// call on GameFrame
-		new GameFrame();
-	} // end of main method
-} // end of Main class
+    public static void main(String[] args) {
+        // call on GameFrame
+        new GameFrame();
+    } // end of main method
+} // end of Main class//
