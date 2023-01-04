1916a104bdccc252ac267943aac9a651d24ed5f4
Atisa Wang <atisa.wang@student.tdsb.on.ca>
2023-01-03 20:59:03 -0500

    dinosaur game (part 3!)

--- src/Dinosaur.java
+++ src/Dinosaur.java
@@ -1,32 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Dinosaur {
 
-    // change into either flappy bird or dinosaur
+    // ================================================================================
+    // VARIABLES
+    // ================================================================================
 
-
-    /*
-1. ImageIcon
-This is typically used when you have small images that you want to add to buttons or to use as window icons. These may be created directly from anything that implements the Image interface.
-
-2. BufferedImage
-Typically used when you need to manipulate individual pixels within an image, or when you want to double-buffer a custom paint(Graphics g) method. The image resides in RAM, so it can take up a lot of space, and modifications to BufferedImage instances are not typically hardware-accelerated.
-
-3. VolatileImage
-Hardware-accelerated image, so it's fast, but you run the risk of the hardware-backed buffer being overwritten before you're done painting (although this rarely happens and according to Oracle, is only an issue for Windows-based machines). It's a little more difficult to use than BufferedImage for double-buffering custom paint(Graphics g) methods, but is well worth it if you find yourself doing a lot of pre-processing before rendering to screen.
-
-4. Image
-This is basically just an interface that defines some bare-bones functionality that every Image should have. You should use this when you don't need to modify an image's contents and/or want to make methods that handle read-only-image data most flexible.
-     */
-
-    // variables
-
+    // position
     public int x;
     public int y;
+    // dimensions
     public static int WIDTH; // make final later
     public static int HEIGHT; // make final later
-
-
+    // objects
+    public BufferedImage image;
+    public Rectangle dinoBoundary;
 
 
 }
--- src/Score.java
+++ src/Score.java
@@ -1,2 +1,45 @@
 public class Score {
+
+    // ================================================================================
+    // VARIABLES
+    // ================================================================================
+    private int score;
+    private static int highScore = 0;
+
+    // ================================================================================
+    // CONSTRUCTOR
+    // ================================================================================
+    public Score() {
+        score = 0;
+    }
+
+    // ================================================================================
+    // METHODS
+    // ================================================================================
+
+    draw(Graphics g){
+
+    }
+
+    // getters
+    public int getScore() {
+        return score;
+    }
+
+    public static int getHighScore() {
+        return highScore;
+    }
+
+    // setters
+    public void setScore(int score){
+        this.score = score;
+    }
+
+    // update high score if current score is greater than previous high score
+    public void updateHighScore() {
+        if (score > highScore) {
+            highScore = score;
+        }
+    }
+
 }
