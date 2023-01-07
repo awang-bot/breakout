45eb5f478334d7862370ff56694479c49e070b4d
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2023-01-07 11:10:13 -0500

    create animation basic structure

---
+++ src/Animation.java
@@ -0,0 +1,13 @@
+/* Anne Liu and Atisa Wang
+January 7, 2022
+Animation
+This program will handle the animations of the obstacles and dinosaurs. */
+
+public class Animation
+{
+  
+  
+  
+
+
+} // end of Animation class
