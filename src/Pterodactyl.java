f3b0e6a35dc34f435881aa0e243b8849cee927aa
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2023-01-07 10:46:01 -0500

    Update Pterodactyl.java

--- src/Pterodactyl.java
+++ src/Pterodactyl.java
@@ -1,1 +1,38 @@
+/* Anne Liu and Atisa Wang
+January 7, 2022
+Pterodactyl
+This class will manage the characteristics of the pterodactyl obstacle. */
 
+import java.awt.*; // I'm not sure if we use this
+
+public class Pterodactyl extends Rectangle
+{
+
+  // variable declaration
+  public int[] heightOffGroundArr; // pterodactyl height off the ground
+  int heightOffGround;
+  String birdImageURL→ {10, 20, 30} (or smth like that)
+  public static final int BIRDHEIGHT, birdWidth; //width, height of object itself
+
+  
+  // constructor
+  public Pterodactyl()
+  {
+    
+
+  }
+  
+  public void move()
+  {
+    x-=1;
+  }
+
+  // draws the current location of the pterodactyl to the screen
+  public void draw(Graphics g) {
+    
+		g.setColor(Color.white);
+		g.fillRect(x, y, BALL_DIAMETER, BALL_DIAMETER);
+	} // end of draw
+  
+
+}
