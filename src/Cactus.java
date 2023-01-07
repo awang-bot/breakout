37857fae13cdd452eef14a7e36e9506c12a48c25
Atisa Wang <atisa.wang@student.tdsb.on.ca>
2023-01-07 12:30:37 -0500

    cactus formatting

--- src/Cactus.java
+++ src/Cactus.java
@@ -1,14 +1,17 @@
-/* Anne Liu and Atisa Wang
-January 7, 2023
-Cactus
-This class will manage the characteristics of the cactus obstacle. */
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * <pre>
+ * Anne Liu and Atisa Wang
+ * January 7, 2023
+ *
+ * CACTUS
+ * This class manages the characteristics and methods of the cactus obstacle. </pre>
+ */
 public class Cactus extends Rectangle {
     // ================================================================================
     // VARIABLES
@@ -22,27 +25,37 @@ public class Cactus extends Rectangle {
      */
     public static final String[][] CACTUS_IMAGES = {{"1", "resources/cactus1.png", "34", "70"}, {"2", "resources/cactus2.png", "68", "70"}, {"3", "resources/cactus3.png", "102", "70"}, {"4", "resources/cactus4.png", "50", "96"}, {"5", "resources/cactus5.png", "100", "96"}, {"6", "resources/cactus6.png", "150", "96"}};
 
+    /**
+     * Cactus identifiers: 1 - one small, 2 - two small, 3 - three small, 4 - one big, 5 - two big, 6 - group of four
+     */
     public int id;
-    public String imageURL;
+    public String imagePath;
     final BufferedImage buffImage;
 
     // ================================================================================
     // CONSTRUCTOR
     // ================================================================================
-    public Cactus(int cactusNum, int x, int y) {
-        super(x, y, Integer.parseInt(CACTUS_IMAGES[cactusNum][2]), Integer.parseInt(CACTUS_IMAGES[cactusNum][3]));
+    public Cactus(int index, int x, int y) {
+        super(x, y, Integer.parseInt(CACTUS_IMAGES[index][2]), Integer.parseInt(CACTUS_IMAGES[index][3]));
 
-        id = Integer.parseInt(CACTUS_IMAGES[cactusNum][0]); // determine the design number
-        imageURL = CACTUS_IMAGES[cactusNum][1]; // determine the URL of the image from the array
+        id = Integer.parseInt(CACTUS_IMAGES[index][0]); // set ID
+        imagePath = CACTUS_IMAGES[index][1]; // set image path
+        // initialize buffImage
         try {
-            buffImage = ImageIO.read(new File(imageURL));
+            buffImage = ImageIO.read(new File(imagePath));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    // ================================================================================
+    // METHODS
+    // ================================================================================
+
+    /**
+     * move the cactus 5px left
+     */
     public void move() {
-        // move 5 pixels to the left
         x -= 5; // speeds up later? maximum speed? or maybe the speed boost will be coded in the gamepanel move()?
     }
 
@@ -53,7 +66,6 @@ public class Cactus extends Rectangle {
         g2d.drawRect(x, y, width, height); // draw rectangle
         g2d.drawImage(buffImage, x, y, null); // draw cactus png
 
-    } // end of draw method
+    }
 
-
-} // end of Cactus class
+}
