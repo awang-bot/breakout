df02fc202787e8777ac46cc99c115574792516c6
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2023-01-08 20:56:39 -0500

    it jumps now

--- src/Dinosaur.java
+++ src/Dinosaur.java
@@ -1,22 +1,135 @@
+/*
+ * Anne Liu and Atisa Wang
+ * January 8, 2023
+ * Dinosaur
+ * This class will manage the behaviours and characteristics of the dinosaur. 
+ */
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.event.*;
 
-public class Dinosaur {
+public class Dinosaur extends Rectangle {
 
     // ================================================================================
     // VARIABLES
     // ================================================================================
 
-    // TODO add 2d array for different dino modes
-    public int x;
+	/**
+	 * movement speed of dinosaur jumping up/down
+	 */
+	public int yVelocity = 1;
+    // position
+    public static final int x = 100; //FIXME: fix according to the screensize later, should not be manual (#)
     public int y;
-    public int speedX;
+    // dimensions
+    public int width; // make final later
+    public int height; // make final later
+    // objects
     public BufferedImage image;
     public Rectangle dinoBoundary;
+    public boolean up;
+    /**
+     * when true, the dino is still in the air jumping.
+     */
+    public boolean continueJump;
+    public static final int UPPER_BOUND = 400;
+    public static final int LOWER_BOUND = 500;
+    
+    // TODO: create an array for the dinosaur graphics
+    // TODO: figure out the animations...
 
-
-    public static int getSpeedX() {
-        return speedX();
+    // ================================================================================
+    // CONSTRUCTOR
+    // ================================================================================
+    public Dinosaur(int width, int height)
+    {
+//    	super(x, 500, width, height); // the y-coord should be the ground's height
+    	this.width = width;
+    	this.height = height;
+    	y=500; // initialize y to be 500 to start there
+    	yVelocity = -5;
+    	up= true;
+    	continueJump = true;
     }
+    
+    
+    // ================================================================================
+    // METHODS
+    // ================================================================================
+    
+    /** 
+     * updates the direction of the dinosaur based on user input
+     */
+ 	public void keyPressed(KeyEvent e) {
+ 		if (e.getKeyCode() == 32) {
+ 			continueJump = true;
+ 			up = true;
+ 		}
+// 			setYDirection(SPEED * -1);
+// 			yVelocity *=-1;
+ 	 		
+ 	} // end of keyPressed
+    
+ 	/**
+ 	 * Makes the dinosaur stop moving in that direction
+ 	 */
+// 	public void keyReleased(KeyEvent e) {
+// 		if (e.getKeyCode() == 32) {
+// 			yVelocity *=-1;
+// 			move();
+// 		}
+// 	} // end of keyReleased
+ 	
+ 	public void midJump()
+ 	{
+ 		if (continueJump && (y<=LOWER_BOUND && y>=UPPER_BOUND)) {
+	 		move();
+ 		}
+ 	}
+ 	
+ 	/**
+ 	 * Move the dinosaur
+ 	 */
+ 	public void move()
+ 	{
+// 		if (y<=500 && y>=400)
+// 		{
+	 		if (up)
+	 		{
+//	 			continueJump = true;
+	 			y += yVelocity; // add to go back down, minus to go back up
+//	 			up = false;
+	 			if (y == UPPER_BOUND)
+	 			{
+	 				up = false;
+	 				yVelocity*=-1;
+	 			}
+	 		}
+	 		else if (!up) {
+//	 			up = true;
+//	 			yVelocity*=-1;
+	 			y+=yVelocity;
+	 			if (y == LOWER_BOUND)
+	 			{
+//	 				up = true;
+	 				yVelocity*=-1;
+	 				continueJump = false;
+	 			}
+	 		
+ 		}
+// 		}
+ 	} // end of move
+ 	
+ 	
+ 	/**
+ 	 * draws the current location of the paddle to the screen
+ 	 */
+ 	public void draw(Graphics g) {
+ 		g.setColor(Color.black);
+ 		g.fillRect(x, y, width, height);
+ 	} // end of draw
+ 	
+ 	
 }
