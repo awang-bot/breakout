383b2769baefc385516f55f1f302b11b990e8926
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2023-01-08 21:01:13 -0500

    Update Pterodactyl.java

--- src/Pterodactyl.java
+++ src/Pterodactyl.java
@@ -1,5 +1,6 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -23,17 +24,19 @@ public class Pterodactyl extends Rectangle {
      * The second column is the y-coordinate.
      */
 //    public static final String[][] PTERODACTYL_ARR = {{"7", "low"}, {"8", "mid"}, {"9", "high"}};
-    public static final int[][] PTERODACTYL_ARR = {{7, 100}, {8, 150}, {9, 150}}; // FIXME: check when the first index is used... i don't think it's ever
+    public static final int[][] PTERODACTYL_ARR = {{1, 100}, {2, 150}, {3, 150}}; // FIXME: check when the first index is used... i don't think it's ever
+
 
     // FIXME replace [][1] with actual y-coordinate later
-    /**
-     * Filepath to pterodactyl_up.png.
+     /** 
+      * Filepath to pterodactyl_up.png.
      */
     public final static String FILEPATH_UP = "resources/pterodactyl_up.png";
     /**
      * Filepath to pterodactyl_down.png.
      */
     public final static String FILEPATH_DOWN = "resources/pterodactyl_down.png";
+    
     /**
      * Pterodactyl PNG is 80px tall.
      */
@@ -54,6 +57,12 @@ public class Pterodactyl extends Rectangle {
      * Pterodactyl y-coordinate
      */
     public int y;
+    /**
+     * Pterodactyl image
+     */
+    // i don't think we need the imagepath/buffimage
+//    public final String imagePath;
+//    public final BufferedImage buffImage;
 
 
     // ================================================================================
@@ -68,9 +77,8 @@ public class Pterodactyl extends Rectangle {
     	super(x, PTERODACTYL_ARR[index][1]);
     	this.x = x;
         y = PTERODACTYL_ARR[index][1];
-    	
         // FIXME: make this efficient??
-
+        
         birdFlap = new Animation(90); // FIXME figure out deltaTime value
 
         try {
@@ -94,28 +102,29 @@ public class Pterodactyl extends Rectangle {
      * move the pterodactyl 5px left
      */
     public void move() {
-        x -= 10;
+        x -= 5;
     }
 
     public void draw(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         
         /* use this to test the functionality of draw*/
-        g.setColor(Color.black);
+        g.setColor(Color.blue);
         g.fillRect(x, y, BIRD_WIDTH, BIRD_HEIGHT);
         
-//        g2d.setColor(new Color(0f, 0f, 0f, 0f)); // transparent colour
-////        g2d.drawRect(x, y, width, height); // draw rectangle
-//        g2d.drawRect(x, y, BIRD_WIDTH, BIRD_HEIGHT); 
+        // FIXME: draw it from the ground and up, not just down from the top y coord
+        g2d.setColor(new Color(0f, 0f, 0f, 0f)); // transparent colour
+        g2d.drawRect(x, y, width, height); // draw rectangle
+        g2d.drawRect(x, y, BIRD_WIDTH, BIRD_HEIGHT); 
         // TODO add pterodactyl flying animation with BufferedImage and whatnot
     }
 
-    public Rectangle getBounds(){
-        return null; //????
-    }
+//    public Rectangle getBounds(){
+//        return null; //????
+//    }
 
-    public void outOfScreen(){
-        // TODO if x < GAME_WIDTH ...
-    }
+//    public void outOfScreen(){
+//        // TODO if x < GAME_WIDTH ...
+//    }
 
 }
