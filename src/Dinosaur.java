6d7e6b61da34e156544707ea51272047e1ccccaf
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2023-01-13 09:05:03 -0500

    Update Dinosaur.java

--- src/Dinosaur.java
+++ src/Dinosaur.java
@@ -15,25 +15,28 @@ public class Dinosaur extends Rectangle {
     // VARIABLES
     // ================================================================================
 
-    public static final int x = 100;
     /**
      * movement speed of dinosaur jumping up/down
      */
     public int yVelocity;
-    /**
-     * when true, the dino is still in the air jumping.
-     */
-    public static final int UPPER_BOUND = 300;
-    public static final int LOWER_BOUND = 500;
+    public static final int x = 100; //FIXME: fix according to the screensize later, should not be manual (#)
+    public BufferedImage image;
     public int state;
     public static final int START_STATE = 0;
     public static final int NORM_RUN_STATE = 1;
     public static final int JUMP_STATE = 2;
     public static final int CROUCH_STATE = 3;
     public static final int DEAD_STATE = 4;
-    public BufferedImage image;
-    public Animation normal_animation;
-    public Animation crouch_animation;
+    Animation normal_animation;
+    Animation crouch_animation;
+    /**
+     * when true, the dino is still in the air jumping.
+     */
+    public static final int UPPER_BOUND = 300;
+    public static final int LOWER_BOUND = 100;
+
+    // TODO: create an array for the dinosaur graphics
+    // TODO: figure out the animations...
 
     // ================================================================================
     // CONSTRUCTOR
@@ -82,21 +85,31 @@ public class Dinosaur extends Rectangle {
                 break;
             }
             case CROUCH_STATE: {
-                this.y = 326;
+                this.y = 10;
                 crouch_animation.updateFrame();
                 image = crouch_animation.getFrame();
                 break;
             }
             case DEAD_STATE: {
+                this.y = 20;
                 image = Resource.getResourceImage("resources/dino_dead.png");
                 break;
             }
         }
 
-        this.y = image.getTileGridYOffset(); // FIXME
         this.width = image.getWidth();
         this.height = image.getHeight();
 
+    }
+
+    public void jump() {
+        y += yVelocity;
+        if (y >= UPPER_BOUND) {
+            yVelocity *= -1;
+        } else if (y <= LOWER_BOUND) {
+            state = NORM_RUN_STATE;
+            yVelocity *= -1;
+        }
     }
 
     public void draw(Graphics g) {
@@ -129,19 +142,20 @@ public class Dinosaur extends Rectangle {
         }
     }
 
+    /**
+     * @return a Rectangle with the Dinosaur's position and dimensions.
+     */
+    public Rectangle getJumpBounds() {
+        Rectangle rect = new Rectangle();
+        rect.x = this.x;
+        rect.y = this.y;
+        rect.width = this.image.getWidth();
+        rect.height = this.image.getHeight();
+        return rect;
+    }
+
     // ================================================================================
     // HELPER METHODS
     // ================================================================================
-
-    private void jump() {
-        this.y += yVelocity;
-        if (this.y >= UPPER_BOUND) {
-            yVelocity *= -1;
-        } else if (this.y <= LOWER_BOUND) {
-            state = NORM_RUN_STATE;
-            yVelocity *= -1;
-        }
-    }
-
 
 }
