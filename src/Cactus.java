bc363bb6cc752d86d91cffaeb65435e6d9bc60e3
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2023-01-15 11:10:16 -0500

    updated heights according to pixels

--- src/Cactus.java
+++ src/Cactus.java
@@ -19,7 +19,7 @@ public class Cactus extends Rectangle {
      * The second column is the width (px).
      * The third column is the height (px).
      */
-    public static final String[][] CACTUS_ARR = {{"cactus/cactus1.png", "34", "89"}, {"cactus/cactus2.png", "68", "89"}, {"cactus/cactus3.png", "102", "89"}, {"cactus/cactus4.png", "50", "103"}, {"cactus/cactus5.png", "100", "103"}, {"cactus/cactus6.png", "150", "103"}};
+    public static final String[][] CACTUS_ARR = {{"cactus/cactus1.png", "34", "69"}, {"cactus/cactus2.png", "68", "70"}, {"cactus/cactus3.png", "102", "70"}, {"cactus/cactus4.png", "50", "100"}, {"cactus/cactus5.png", "100", "100"}, {"cactus/cactus6.png", "150", "100"}};
     public final String fileName;
     public final BufferedImage buffImage;
     private int xVelocity;
@@ -31,7 +31,7 @@ public class Cactus extends Rectangle {
     	// initialize the cactus position and dimensions
         this.x = x;
         xVelocity = 5;
-        y = GamePanel.LAND_HEIGHT-2*Integer.parseInt(CACTUS_ARR[index][2]); 
+        y = GamePanel.LAND_HEIGHT+14-Integer.parseInt(CACTUS_ARR[index][2]); 
         width = Integer.parseInt(CACTUS_ARR[index][1]);
         height = Integer.parseInt(CACTUS_ARR[index][2]);
 
