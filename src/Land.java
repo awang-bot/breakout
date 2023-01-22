4fca25ad75f469e4e56d1c20327c8ecd3530349a
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2023-01-22 11:35:33 -0500

    Update Land.java

--- src/Land.java
+++ src/Land.java
@@ -1,3 +1,10 @@
+/*
+ * Anne Liu and Atisa Wang
+ * January 11, 2023
+ * Land
+ * This class will manage the land's characteristics and behaviours to be displayed on the screen by GamePanel
+ */
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
@@ -9,8 +16,8 @@ public class Land {
 
     public static final int LAND_POS_Y = 380;
     private int xVelocity;
-    public static final String FILEPATH = "background/land.png";
-    public BufferedImage image;
+    private static final String FILEPATH = "background/land.png";
+    private BufferedImage image;
     public static final int LAND_WIDTH = 2400;
     public int x;
 
@@ -27,21 +34,35 @@ public class Land {
     // METHODS
     // ================================================================================
 
+    /** 
+     * move the land to the left
+     */
     public void move() {
         x -= xVelocity;
     }
     
+    /**
+     * allow GamePanel to set the starting x-coordinate of the land
+     * @param x
+     */
     public void setX(int x)
     {
     	this.x=x;
     }
 
+    /**
+     * draw the land onto the sceren
+     * @param g
+     */
     public void draw(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-
         g2d.drawImage(image, x, LAND_POS_Y, null);
     }
 
+    /** 
+     * allow GamePanel to set the xVelocity as it will change throughout the game
+     * @param speed
+     */
     public void setXVelocity(int speed){
         xVelocity = speed;
     }
