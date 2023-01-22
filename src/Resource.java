1a5936243503b91fbcb022f41540ae927abbb886
anne-liu17 <121645689+anne-liu17@users.noreply.github.com>
2023-01-22 11:38:34 -0500

    Update Resource.java

--- src/Resource.java
+++ src/Resource.java
@@ -1,35 +1,44 @@
+/*
+ * Anne Liu and Atisa Wang
+ * January 16, 2023
+ * Resource
+ * This class will manage the resourcse (i.e. images, fonts) used throughout the program.
+ */
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Resource {
+public class Resource { // this class is primarily for efficiency
 
-    public static BufferedImage getResourceImage(String name) {
-        String path = "resources/"+name;
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File(path));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return img;
-    }
+	public Resource() {
+	}
 
-    public static Font getResourceFont(String name, float size){
-        String path = "resources/"+name;
-        Font font = null;
-        try {
-            font = Font.createFont(Font.TRUETYPE_FONT, new File(path)).deriveFont(size);
-        } catch (FontFormatException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return font;
-    }
+	// images
+	public static BufferedImage getResourceImage(String name) {
+		String path = "resources/" + name; // get the path of the image
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return img; // return the image
+	}
+
+	public static Font getResourceFont(String name, float size) {
+		String path = "resources/" + name; // get the path of the font
+		Font font = null;
+		try {
+			font = Font.createFont(Font.TRUETYPE_FONT, new File(path)).deriveFont(size);
+		} catch (FontFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return font; // return the font
+	}
 
 }
